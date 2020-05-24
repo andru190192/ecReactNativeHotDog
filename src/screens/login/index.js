9f4dbafb893531e23448e2f60406b156618f668a
Gomez Frank <fdgomez@pichincha.com>
2020-05-24 16:16:12 -0500

    add screen login

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, TouchableOpacity, Text} from 'react-native';
+import LottieView from 'lottie-react-native';
+import {Item, Input} from 'native-base';
 
 const LoginScreen = () => {
   return (
@@ -7,10 +10,80 @@ const LoginScreen = () => {
       style={{
         backgroundColor: 'white',
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
       }}>
-      <Text>"Hola Ome"</Text>
+      <View style={{flex: 1, height: 30}}>
+        <LottieView
+          source={require('../../res/animations/hotdog.json')}
+          autoPlay
+          loop
+        />
+      </View>
+      <View style={{marginHorizontal: 34, marginBottom: 34}}>
+        <Item>
+          <Input
+            placeholder="Ingrese usuario"
+            autoCorrect={false}
+            returnKeyType="done"
+          />
+        </Item>
+        <Item>
+          <Input
+            placeholder="Ingrese contraseña"
+            autoCorrect={false}
+            returnKeyType="done"
+            secureTextEntry
+          />
+        </Item>
+      </View>
+      <View style={{flex: 2}}>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#47B27D',
+            marginHorizontal: 34,
+            height: 45,
+            justifyContent: 'center',
+            textAlign: 'center',
+            borderRadius: 10,
+            marginVertical: 10,
+          }}>
+          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>
+            Login
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#1273DE',
+            marginHorizontal: 34,
+            height: 45,
+            justifyContent: 'center',
+            textAlign: 'center',
+            borderRadius: 10,
+            marginVertical: 10,
+          }}>
+          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>
+            Ingresar con Google
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#626B75',
+            marginHorizontal: 34,
+            height: 45,
+            justifyContent: 'center',
+            textAlign: 'center',
+            borderRadius: 10,
+          }}>
+          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>
+            Ingresar con GitHub
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
