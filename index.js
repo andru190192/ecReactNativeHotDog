aa2c41725ba426fd41f04d54d61f9598ab3eb708
Carlos Andres Guachisaca Apolo <cguachis@pichincha.com>
2020-06-03 11:19:48 -0500

    initializate crashlytics and analytics

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import {name as appName} from './app.json';
 import crashlytics from '@react-native-firebase/crashlytics';
 import analytics from '@react-native-firebase/analytics';
 
-//crashlytics().setCrashlyticsCollectionEnabled(!__DEV__);
-//analytics().setAnalyticsCollectionEnabled(!__DEV__);
-//require('react-native').unstable_enableLogBox();
+crashlytics().setCrashlyticsCollectionEnabled(true);
+analytics().setAnalyticsCollectionEnabled(true);
+require('react-native').unstable_enableLogBox();
 
 AppRegistry.registerComponent(appName, () => App);
