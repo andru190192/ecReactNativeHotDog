c993d7fb6fa5d18ae8c6897d1a73c42f671eb8d0
Gomez Frank <fdgomez@pichincha.com>
2020-06-04 22:05:37 -0500

    vector icons install

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import auth from '@react-native-firebase/auth';
 import crashlytics from '@react-native-firebase/crashlytics';
 import LoginScreen from '../../screens/login';
-import { AuthContext } from '../../contexts/authContext';
+import {AuthContext} from '../../contexts/authContext';
 
 const Login = ({navigation}) => {
   // Set an initializing state whilst Firebase connects
@@ -10,18 +10,20 @@ const Login = ({navigation}) => {
   const [user, setUser] = useState();
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
-  const { setUserId } = useContext(AuthContext)
+  const {setUserId} = useContext(AuthContext);
 
   // Handle user state changes
   function onAuthStateChanged(user) {
     setUser(user);
-    if (initializing) setInitializing(false);
+    if (initializing) {
+      setInitializing(false);
+    }
   }
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
-  }, []);
+  }, [onAuthStateChanged]);
 
   useEffect(() => {
     if (user) {
@@ -30,42 +32,44 @@ const Login = ({navigation}) => {
         screen: 'Profile',
       });
     }
-  }, [user]);
+  }, [navigation, setUserId, user]);
 
   function onHandlerLogin() {
     auth()
-        .signInWithEmailAndPassword(username, password)
-        .then((userLogin) => {
-          console.warn('User account created & signed in!',userLogin);
-          setUserId(userLogin.user.uid);
-          navigation.navigate('Home', {
-            screen: 'Profile',
-          });
-          crashlytics().setUserId(userLogin.user.uid);
-        })
-        .catch(error => {
-          if (error.code === 'auth/email-already-in-use') {
-            console.warn('That email address is already in use!');
-          }
-
-          if (error.code === 'auth/invalid-email') {
-            console.warn('That email address is invalid!');
-          }
-
-          console.error(error);
+      .signInWithEmailAndPassword(username, password)
+      .then(userLogin => {
+        console.warn('User account created & signed in!', userLogin);
+        setUserId(userLogin.user.uid);
+        navigation.navigate('Home', {
+          screen: 'Profile',
         });
+        crashlytics().setUserId(userLogin.user.uid);
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.warn('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.warn('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
   }
 
-  if (initializing) return null;
+  if (initializing) {
+    return null;
+  }
 
   if (!user) {
     return (
       <LoginScreen
-          onHandlerLogin={onHandlerLogin}
-          username={username}
-          password={password}
-          setUsername={(username) => setUsername(username)}
-          setPassword={(password) => setPassword(password)}
+        onHandlerLogin={onHandlerLogin}
+        username={username}
+        password={password}
+        setUsername={username => setUsername(username)}
+        setPassword={password => setPassword(password)}
       />
     );
   }
--- src/container/profile/profile.js
+++ src/container/profile/profile.js
@@ -1,43 +1,41 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import ProfileScreen from '../../screens/profile';
-import { AuthContext } from '../../contexts/authContext';
+import {AuthContext} from '../../contexts/authContext';
 
 const Profile = ({navigation}) => {
-
-  const { userId } = useContext(AuthContext);
+  const {userId} = useContext(AuthContext);
   const [user, setUser] = useState();
 
   useEffect(() => {
     const subscriber = firestore()
-        .collection('users')
-        .doc(userId)
-        .onSnapshot(documentSnapshot => {
-          console.log('User data: ', documentSnapshot.data());
-          setUser(documentSnapshot.data());
-          console.log(user);
-        });
+      .collection('users')
+      .doc(userId)
+      .onSnapshot(documentSnapshot => {
+        console.log('User data: ', documentSnapshot.data());
+        setUser(documentSnapshot.data());
+        console.log(user);
+      });
 
     // Stop listening for updates when no longer required
     return () => subscriber();
-  }, []);
+  }, [user, userId]);
 
   const onHandlerLogout = () => {
-    auth().signOut().then(() => {
-      console.warn('User signed out!');
-      navigation.navigate('Login');
-    });
+    auth()
+      .signOut()
+      .then(() => {
+        console.warn('User signed out!');
+        navigation.navigate('Login');
+      });
+  };
+
+  if (user === undefined) {
+    return null;
   }
 
-  if (user === undefined) return null
-
-  return (
-      <ProfileScreen
-        user={user}
-        onHandlerLogout={onHandlerLogout}
-      />
-  );
+  return <ProfileScreen user={user} onHandlerLogout={onHandlerLogout} />;
 };
 
 export default Profile;
